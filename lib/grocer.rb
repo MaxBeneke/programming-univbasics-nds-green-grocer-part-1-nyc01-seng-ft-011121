687873e2e5bb53944557a6e9e055e848bfedbd6f
Max Beneke <max.beneke@gmail.com>
2020-12-24 14:49:11 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,16 +1,30 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require 'pry'
 
+def find_item_by_name_in_collection(name, collection)
+  final_hash = {}
+  collection.each do |item|
+    item.each do |attribute, value|
+      if value == name
+        final_hash = item
+      end
+   end
+  end
+  if final_hash == {}
+    NIL
+  else
+  final_hash
+end
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  final_hash = {}
+  binding.pry
+  cart.each do |whole_item| 
+    item_count = cart.count(whole_item[:item])
+      whole_item[:count] = item_count
+      # successfully adds the count, but my item_count is 0 everytime
+  end
+  new_cart = cart.uniq
 end
 
 
