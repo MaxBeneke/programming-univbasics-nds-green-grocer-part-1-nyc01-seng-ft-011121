abee597a781c466529c4c39a1bed89ed41f676a3
Max Beneke <max.beneke@gmail.com>
2020-12-24 14:54:05 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,7 +20,7 @@ def consolidate_cart(cart)
   final_hash = {}
   cart.each do |whole_item| 
     item_count = cart.count(whole_item[:item])
-      item_count = whole_item[:count]
+      item_count = whole_item[:count] + 1
       # successfully adds the count, but my item_count is 0 everytime
   end
   new_cart = cart.uniq
