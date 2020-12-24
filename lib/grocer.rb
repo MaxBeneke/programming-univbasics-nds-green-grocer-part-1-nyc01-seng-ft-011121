75a0f463d62909b86350cc6b3dc5d8265d873b35
Max Beneke <max.beneke@gmail.com>
2020-12-24 14:55:20 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,7 +20,6 @@ def consolidate_cart(cart)
   final_hash = {}
   cart.each do |whole_item| 
     item_count = cart.count(whole_item[:item]) + 1
-    binding.pry
       whole_item[:count] = item_count
       # successfully adds the count, but my item_count is 0 everytime
   end
